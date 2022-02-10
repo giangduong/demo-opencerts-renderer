a964217a5ab29925773006ac984e22f12d71d6be
Sim Boon Long <simboonlong@gmail.com>
2022-02-10 12:18:19 +0800

    fix: govtech demo cert styles (#378)

--- src/templates/govtech-demo-cert/certificate.tsx
+++ src/templates/govtech-demo-cert/certificate.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import { get } from "lodash";
+import styled from "@emotion/styled";
 import React, { FunctionComponent } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./common/demo-styles.css";
@@ -10,76 +11,109 @@ import mainLogo from "./common/opencerts-logo.svg";
 import logo from "./common/govtech-logo.png";
 import certificateBg from "./common/certificate-background.png";
 
+const Page = styled("div")<{ certificateBg: string }>`
+  max-width: 297mm;
+  margin: 0 auto;
+
+  position: relative;
+  background-image: ${props => props.certificateBg};
+  background-position: center;
+  background-size: cover;
+  border: 10px solid #324353;
+
+  .logo-oc {
+    max-width: 100%;
+    width: 320px;
+    @media (min-width: 1024px) {
+      width: 600px;
+    }
+  }
+
+  .logo-gt {
+    max-width: 100%;
+    width: 200px;
+    @media (min-width: 1024px) {
+      width: 300px;
+    }
+  }
+
+  .text-sm {
+    font-size: 12px;
+    @media (min-width: 1024px) {
+      font-size: 21px;
+    }
+  }
+
+  .text-md {
+    font-size: 16px;
+    @media (min-width: 1024px) {
+      font-size: 26px;
+    }
+  }
+
+  .text-lg {
+    font-size: 24px;
+    @media (min-width: 1024px) {
+      font-size: 34px;
+    }
+  }
+
+  footer {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 64px 24px;
+
+    @media (min-width: 1024px) {
+      padding: 96px;
+    }
+  }
+
+  @media print {
+    height: 210mm;
+
+    .logo-oc {
+      width: 600px;
+    }
+
+    .logo-gt {
+      width: 300px;
+    }
+  }
+`;
+
 export const CertificateTemplate: FunctionComponent<TemplateProps<GovtechOpencertsTemplateCertificate>> = ({
   document
 }) => (
-  <>
+  <Page certificateBg={`url('${certificateBg}')`} className="p-4">
     <PrintWatermark />
-    <div
-      className="p-2 container"
-      style={{
-        backgroundImage: `url('${certificateBg}')`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        border: "10px solid #324353"
-      }}
-    >
-      <div
-        className="p-2"
-        style={{
-          border: "0px solid #324353"
-        }}
-      >
-        <div className="my-5 m-lg-5 text-center">
-          <img src={mainLogo} alt="OpenCerts Logo" style={{ width: "60%", height: "auto", maxWidth: "1000px" }} />
-        </div>
-        <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-body" style={{ textAlign: "center" }}>
-          <i>This is to certify that</i>
-        </div>
-        <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-title">
-          <b>{document.recipient.name}</b>
-        </div>
-        <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-body" style={{ textAlign: "center" }}>
-          <i>has successfully completed the</i>
-        </div>
-        <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-title" style={{ textAlign: "center" }}>
-          OpenCerts Demo
-        </div>
-        <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-body" style={{ textAlign: "center" }}>
-          <i>certification through training administered by</i>
-        </div>
-        <div className="row">
-          <div className="col" />
-          <div className="col">
-            <img
-              className="w-100"
-              style={{ width: "100%", height: "auto", minWidth: "100px" }}
-              src={logo}
-              alt="Govtech Logo"
-            />
-          </div>
-          <div className="col" />
-        </div>
-
-        <div
-          className="row"
-          style={{
-            paddingLeft: "8%",
-            paddingTop: "5%"
-          }}
-        >
-          <div className="col text-center transcript">
+    <main className="text-center">
+      <div className="m-4 m-lg-5">
+        <img src={mainLogo} alt="OpenCerts Logo" className="logo-oc" />
+      </div>
+      <div className="m-4 m-lg-5 text-md">
+        <i>This is to certify that</i>
+      </div>
+      <div className="m-4 m-lg-5 text-lg">
+        <b>{document.recipient.name}</b>
+      </div>
+      <div className="m-4 m-lg-5 text-md">
+        <i>has successfully completed the</i>
+      </div>
+      <div className="m-4 m-lg-5 text-lg">OpenCerts Demo</div>
+      <div className="m-4 m-lg-5 text-md">
+        <i>certification through training administered by</i>
+      </div>
+      <img className="logo-gt" src={logo} alt="Govtech Logo" />
+    </main>
+    <footer>
+      <div className="row align-items-center">
+        <div className="col">
+          <div className="text-center text-sm">
             <img
               style={{ width: "100%", height: "auto" }}
               src={get(document, "additionalData.certSignatories[0].signature")}
             />
-            <hr
-              style={{
-                border: "none",
-                height: "1px",
-                backgroundColor: "#333"
-              }}
-            />
+            <hr style={{ backgroundColor: "#333" }} />
             <div>
               <b>{get(document, "additionalData.certSignatories[0].name")}</b>
               <br />
@@ -87,20 +121,12 @@ export const CertificateTemplate: FunctionComponent<TemplateProps<GovtechOpencer
               {get(document, "additionalData.certSignatories[0].organisation")}
             </div>
           </div>
-
-          <div className="col" />
-
-          <div
-            className="d-flex flex-row-reverse col transcript"
-            style={{
-              paddingTop: "5%",
-              paddingRight: "5%"
-            }}
-          >
-            Dated {format(document.issuedOn, "DD/MM/YYYY")}
-          </div>
+        </div>
+        <div className="col" />
+        <div className="col">
+          <div className="text-sm text-right">Dated {format(document.issuedOn, "DD/MM/YYYY")}</div>
         </div>
       </div>
-    </div>
-  </>
+    </footer>
+  </Page>
 );
